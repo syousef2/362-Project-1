4229440145fe978d6a8e722c32724f586f085c34
syousef2 <syousef@hawk.iit.edu>
2025-03-06 21:26:02 -0600

    adding date & time feature

--- js/site.js
+++ js/site.js
@@ -1,1 +1,7 @@
+document.addEventListenr('DOMContentLoaded', function () {
+  const dateElement = document.createElement('p');
+  dateElement.textContent= 'Current Date and Time: ' + new Data().toLocateString();
+  document.body.appendChild(dateElement);
+});  
+
 
