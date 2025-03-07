8e9407725cb274a902e4e53abc58af623c76dc44
syousef2 <syousef@hawk.iit.edu>
2025-03-06 21:31:17 -0600

    spelling error

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
-document.addEventListenr('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {
   const dateElement = document.createElement('p');
-  dateElement.textContent= 'Current Date and Time: ' + new Data().toLocateString();
+  dateElement.textContent= 'Current Date and Time: ' + new Date().toLocaleString();
   document.body.appendChild(dateElement);
 });  
 
